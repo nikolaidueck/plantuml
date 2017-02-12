56999eb61329f30cc11cc754332f63f338fdac60
DUEC007 <DUEC007@GUTNB01283.nat.arvato-services.org>
2017-02-12 19:28:16 +0100

    new Plugin for the dsl

---
+++ net.sourceforge.plantuml.DSL/src/net/sourceforge/plantuml/DSL/DMDStructureDslTextProvider.java
@@ -0,0 +1,46 @@
+package net.sourceforge.plantuml.DSL;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.texteditor.ITextEditor;
+
+import dmd.dsl.DSL;
+import dmd.dsl.DSLException;
+import net.sourceforge.plantuml.text.AbstractDiagramTextProvider;
+
+public class DMDStructureDslTextProvider extends AbstractDiagramTextProvider {
+
+	@Inject Logger logger;
+	
+	public DMDStructureDslTextProvider() {
+		setEditorType(ITextEditor.class);
+	}
+
+	public boolean supportsSelection(ISelection selection) {
+		return false;
+	}
+	
+	@Override
+	protected String getDiagramText(IEditorPart editorPart, IEditorInput editorInput, ISelection ignore) {
+		if (! (editorInput instanceof IFileEditorInput && "groovy".equals(((IFileEditorInput) editorInput).getFile().getFileExtension()))) {
+			return null;
+		}
+		IFile file = ((IFileEditorInput) editorInput).getFile();
+		String result = "";
+		DSL dsl = new DSL();
+		try {
+			result = dsl.generatePlantUml(file.getRawLocation().makeAbsolute().toFile());
+		} catch (DSLException e) {
+			logger.log(Level.SEVERE, "Fehler beim generieren von PlantUml", e);
+		}
+		return (result.length() > 0 ? result : null);
+	}
+}
